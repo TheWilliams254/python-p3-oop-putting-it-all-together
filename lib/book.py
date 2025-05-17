3da2d0154f1fd4b73587eda6d8e4c8572e99aadd
william <williamwambugu663@gmail.com>
2025-05-17 12:02:41 +0300

    @property decorator

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,37 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
+    """A class representing a book with a title and page count."""
     
-        
+    def __init__(self, title, page_count):
+        self.title = title
+        self.page_count = page_count
+
+    def turn_page(self):
+        """Simulates turning a page in the book."""
+        print("Flipping the page...wow, you read fast!")
+
+    @property
+    def page_count(self):
+        """Gets the page count of the book."""
+        return self._page_count
+
+    @page_count.setter
+    def page_count(self, value):
+        if not isinstance(value, int):
+            print("page_count must be an integer")
+        self._page_count = value
+
+    @property
+    def title(self):
+        """Gets the title of the book."""
+        return self._title
+
+    @title.setter
+    def title(self, value):
+        if not isinstance(value, str):
+            raise TypeError("title must be a string")
+        self._title = value
+
+    def __str__(self):
+        return f"{self.title} has {self.page_count} pages"
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,40 @@
 #!/usr/bin/env python3
 
 class Shoe:
+    def __init__(self, brand, size, condition="New"):
+        self._condition = condition
+        self.brand = brand
+        self.size = size
     pass
+
+    @property
+    def condition(self):
+        return self._condition
+
+    @property
+    def brand(self):
+        """Gets the brand of the shoe."""
+        return self._brand
+
+    @brand.setter
+    def brand(self, value):
+        if not isinstance(value, str):
+            raise TypeError("brand must be a string")
+        self._brand = value
+
+    @property
+    def size(self):
+        """Gets the size of the shoe."""
+        return self._size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            print("size must be an integer")
+        self._size = value
+
+    def cobble(self):
+        print("Your shoe is as good as new!")
+        pass
+    
+
